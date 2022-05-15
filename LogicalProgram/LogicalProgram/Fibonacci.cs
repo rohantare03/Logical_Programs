49a78adb32668a69383bd1a0c83709efe27b74a7
rohantare03 <rohatare123@gmail.com>
2022-05-15 19:21:55 +0530

    [Rohan] Add : Finding Fibonacci series

---
+++ LogicalProgram/LogicalProgram/Fibonacci.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Program 
+{
+    internal class Fibonacci
+    {
+        public void FibSeries()
+        {
+            int num1 = 0, num2 = 1, num3, n = 10;
+            
+
+            Console.WriteLine("Fibonacci series is ");
+            Console.Write(num1 + " " + num2 + " ");
+
+            for (int i = 2; i < n; i++)
+            {
+                num3 = num1 + num2;
+                Console.Write(num3 + " ");
+                num1 = num2;
+                num2 = num3;
+            }
+        }
+    }
+}
---
+++ LogicalProgram/LogicalProgram/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Logical_Program
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number for a problem");
+            Console.WriteLine("1 for fibonacci series");
+
+            int Problem = Convert.ToInt32(Console.ReadLine());
+            switch (Problem)
+            {
+                case 1:
+                    Fibonacci fibonacci = new Fibonacci();
+                    fibonacci.FibSeries();
+                    break;
+            }
+            
+        }
+    }
+}
