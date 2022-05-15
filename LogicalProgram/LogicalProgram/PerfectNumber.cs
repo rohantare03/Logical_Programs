dded111f8a6ca65fe7e4766fd60916c5865bbe02
rohantare03 <rohatare123@gmail.com>
2022-05-15 19:33:23 +0530

    [Rohan] Add : Finding the Perfect number

---
+++ LogicalProgram/LogicalProgram/PerfectNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Program
+{
+    internal class PerfectNumber
+    {
+        public void Perfect()
+        {
+            int Num, Sum = 0;
+            Console.WriteLine("Enter a Number : ");
+            Num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i < Num; i++)
+            {
+                if (Num % i == 0)
+                {
+                    Sum = Sum + i;
+                }
+            }
+            if ( Sum == Num)
+            {
+                Console.WriteLine("The Number is a perfect  number.");
+            }
+            else
+            {
+                Console.WriteLine("The Number is not a perfect number.");
+            }
+        }
+    }
+}
--- LogicalProgram/LogicalProgram/Program.cs
+++ LogicalProgram/LogicalProgram/Program.cs
@@ -7,6 +7,7 @@ namespace Logical_Program
         {
             Console.WriteLine("Enter a number for a problem");
             Console.WriteLine("1 for fibonacci series");
+            Console.WriteLine("2 for perfect number");
 
             int Problem = Convert.ToInt32(Console.ReadLine());
             switch (Problem)
@@ -14,6 +15,10 @@ namespace Logical_Program
                 case 1:
                     Fibonacci fibonacci = new Fibonacci();
                     fibonacci.FibSeries();
+                    break;
+                case 2:
+                    PerfectNumber perfectNumber = new PerfectNumber();
+                    perfectNumber.Perfect();
                     break;
             }
             
