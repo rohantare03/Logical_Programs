8975e7030a0d217abab216e9373d9b94479de462
rohantare03 <rohatare123@gmail.com>
2022-05-16 11:13:19 +0530

    [Rohan] Add : Checking the Prime number

---
+++ LogicalProgram/LogicalProgram/PrimeNumber.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    internal class PrimeNumber
+    {
+        public void PrimeNum()
+        {
+            int count = 0;
+            Console.WriteLine("Enter a number");
+            int Num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 2; i <= Num / 2; i++)
+            {
+                if (Num % i == 0)
+                {
+                    count++;
+                    break;
+
+                }
+
+
+            }
+            if (count == 0)
+            {
+                Console.WriteLine("The number is  a prime number.");
+            }
+            else
+            {
+                Console.WriteLine("The number is not a prime number");
+            }
+        }
+    }
+}
--- LogicalProgram/LogicalProgram/Program.cs
+++ LogicalProgram/LogicalProgram/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-namespace Logical_Program
+namespace LogicalProgram 
 {
     public class Program
     {
@@ -8,6 +8,7 @@ namespace Logical_Program
             Console.WriteLine("Enter a number for a problem");
             Console.WriteLine("1 for fibonacci series");
             Console.WriteLine("2 for perfect number");
+            Console.WriteLine("3 for prime number");
 
             int Problem = Convert.ToInt32(Console.ReadLine());
             switch (Problem)
@@ -20,6 +21,10 @@ namespace Logical_Program
                     PerfectNumber perfectNumber = new PerfectNumber();
                     perfectNumber.Perfect();
                     break;
+                case 3:
+                    PrimeNumber primeNumber = new PrimeNumber();
+                    primeNumber.PrimeNum();
+                    break; 
             }
             
         }
